727aff80d1acd65ddc06d3c595e32adf8d9a6052
Kevin Rocker <kevin.j.rocker@gmail.com>
2016-07-28 22:29:20 -0400

    Flesh out install_links.sh

---
+++ install_links.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -s ~/.config/bashrc ~/.bashrc
+ln -s ~/.config/git-completion.bash ~/.git-completion.bash
+ln -s ~/.config/git-prompt.sh ~/.git-prompt.sh
+ln -s ~/.config/git_config ~/.gitconfig
+ln -s ~/.config/xmobar_rc ~/.xmobarrc
+ln -s ~/.config/xmonad.hs ~/.xmonad/xmonad.hs
+
+ln -s ~/Dropbox/Apps/Monospace/ ~/notes
