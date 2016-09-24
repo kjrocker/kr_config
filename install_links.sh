72e4dc6011ede71543c6a9925356cdb489da4529
Kevin Rocker <kevin.j.rocker@gmail.com>
2016-09-24 18:56:22 -0400

    Add an argument and more absolute link paths

--- install_links.sh
+++ install_links.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-REPO_PATH="~/.config/kr_config"
+REPO_PATH="/home/kevin/.config/kr_config"
 
-ln -s ${REPO_PATH}/bashrc ~/.bashrc
-ln -s ${REPO_PATH}/git-completion.bash ~/.git-completion.bash
-ln -s ${REPO_PATH}/git-prompt.sh ~/.git-prompt.sh
-ln -s ${REPO_PATH}/git_config ~/.gitconfig
+case "$1" in
+  help )
+    echo "Supported optional arguments include: xmonad"
+    exit
+    ;;
+  xmonad )
+    ln -s ${REPO_PATH}/xmobar_rc /home/kevin/.xmobarrc
+    ln -s ${REPO_PATH}/xmonad.hs /home/kevin/.xmonad/xmonad.hs
+    ;;
+  openbox )
+    echo "No openbox config files under git at this time."
+    ;;
+esac
 
-ln -s ${REPO_PATH}/xmobar_rc ~/.xmobarrc
-ln -s ${REPO_PATH}/xmonad.hs ~/.xmonad/xmonad.hs
+ln -s ${REPO_PATH}/bashrc /home/kevin/.bashrc
+ln -s ${REPO_PATH}/git-completion.bash /home/kevin/.git-completion.bash
+ln -s ${REPO_PATH}/git-prompt.sh /home/kevin/.git-prompt.sh
+ln -s ${REPO_PATH}/git_config /home/kevin/.gitconfig
